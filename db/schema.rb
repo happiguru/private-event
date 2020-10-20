b041c70e428eb562e7104ccd11dbeed144f7bdec
hguruman <hguruman@gmail.com>
2020-10-21 02:46:42 +0400

    Add Created_id and index to event model and run migration. Test an instance of created event to show on the view

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_20_195213) do
+ActiveRecord::Schema.define(version: 2020_10_20_221520) do
 
   create_table "events", force: :cascade do |t|
     t.string "title"
@@ -19,6 +19,8 @@ ActiveRecord::Schema.define(version: 2020_10_20_195213) do
     t.string "location"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "creator_id"
+    t.index ["creator_id"], name: "index_events_on_creator_id"
   end
 
   create_table "invitations", force: :cascade do |t|
