a6d527a25e0899360935a8db8fa8d899e99dc345
Miguel Uzcátegui <migueluzcategui09@gmail.com>
2020-10-22 13:14:23 -0400

    Add tests for user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,38 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { User.create!(name: 'name', username: 'username') }
+
+  context 'Creating a user' do
+    it 'creates a user in the User model' do
+      expect(user).to be_an User
+    end
+  end
+
+  context 'Test validations' do
+    it 'does not create a user if no username provided' do
+      user.username = ''
+      expect(user).not_to be_valid
+    end
+    
+    it 'does not create a user if no name provided' do
+      user.name = ''
+      expect(user).not_to be_valid
+    end
+  end
+
+  context 'Association test for user/event' do
+    it "should have many created_events" do
+      t = User.reflect_on_association(:created_events)
+      expect(t.macro).to eq(:has_many)
+    end
+    it "should have many attended_events" do
+      t = User.reflect_on_association(:attended_events)
+      expect(t.macro).to eq(:has_many)
+    end
+    it "should have many invitations" do
+      t = User.reflect_on_association(:invitations)
+      expect(t.macro).to eq(:has_many)
+    end
+  end
 end
