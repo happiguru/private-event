4d69389ed5576a4ece1ea210362606180d79392e
hguruman <hguruman@gmail.com>
2020-10-23 18:54:09 +0400

    Update rspec

--- spec/views/users/show.html.erb_spec.rb
+++ spec/views/users/show.html.erb_spec.rb
@@ -1,15 +1,1 @@
 require 'rails_helper'
-
-RSpec.describe 'users/show', type: :view do
-  before(:each) do
-    @user = assign(:user, User.create!(
-                            name: 'Name',
-                            username: 'Username'
-                          ))
-  end
-
-  it 'renders attributes in <h1>' do
-    render
-    expect(rendered).to match(/Name/)
-  end
-end
